8f9830c16b5e37c2e9876d431a2f13ad7e6bcac5
kushak <ship93@yandex.ru>
2017-02-20 15:10:11 +0300

    Initial Commit

---
+++ Currency conwerter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Currency conwerter
+//
+//  Created by user on 20.02.17.
+//  Copyright © 2017 Oleg Shipulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

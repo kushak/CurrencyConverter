0e932b52424a7caacd658015fda4cbce3cce4c97
kushak <ship93@yandex.ru>
2017-02-22 09:29:04 +0300

    fix

--- Currency conwerter/ViewController.swift
+++ Currency conwerter/ViewController.swift
@@ -75,6 +75,46 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         self.requestCurrentCurrencyRate()
     }
     
+    func requestCurrentCurrencyRate() {
+        self.activityIndicator.startAnimating()
+        self.label.text = ""
+        
+        let baseCurrencyIndex = self.pickerFrom.selectedRow(inComponent: 0)
+        let toCurrencyIndex = self.pickerTo.selectedRow(inComponent: 0)
+        
+        let baseCurrency = self.currencies[baseCurrencyIndex]
+        let toCurrency = self.currenciesExceptBase()[toCurrencyIndex]
+        
+        self.retrieveCurrencyRate(baseCurrency: baseCurrency, toCurrency: toCurrency) {[weak self] (value) in
+            DispatchQueue.main.async(execute: {
+                if let strongSelf = self {
+                    if strongSelf.label.text != value {
+                        strongSelf.label.text = value
+                    }
+                    strongSelf.activityIndicator.stopAnimating()
+                    strongSelf.pickerFrom.reloadAllComponents()
+                    strongSelf.pickerTo.reloadAllComponents()
+                }
+            })
+        }
+    }
+    // retrieve - извлечь
+    func retrieveCurrencyRate(baseCurrency: String, toCurrency: String, complition: @escaping (String) -> Void) {
+        self.requestCurrencyRates(baseCurrency: baseCurrency) {[weak self] (data, error) in
+            var string = "No currency retieved!"
+            
+            if let currentError = error {
+                string = currentError.localizedDescription
+            } else {
+                if let strongSelf = self {
+                    string = strongSelf.parseCurrencyRatesResponse(data: data, toCurrency: toCurrency)
+                }
+            }
+            
+            complition(string)
+        }
+    }
+    
     func requestCurrencyRates(baseCurrency: String, parseHandler: @escaping (Data?, Error?) -> Void) {
         let url = URL(string: "https://api.fixer.io/latest?base=" + baseCurrency)!
         
@@ -86,14 +126,15 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         dataTask.resume()
     }
     
-    func parseCurrencyRatesResponse(data: Data?, baseCurrency: String, toCurrency: String) -> String {
+    func parseCurrencyRatesResponse(data: Data?, toCurrency: String) -> String {
         var value: String = ""
         
         do {
             let json = try JSONSerialization.jsonObject(with: data!, options: []) as? Dictionary<String, Any>
             if let parsedJSON = json {
 //                print("\(parsedJSON)")
-                if let rates = parsedJSON["rates"] as? Dictionary<String, Double> {
+                if let rates = parsedJSON["rates"] as? Dictionary<String, Double>,
+                    let baseCurrency = parsedJSON["base"] as? String {
                     
                     if self.rates.count != 0 {
                         self.rates = Array(rates.values)
@@ -119,46 +160,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         }
         
         return value
-    }
-    
-    func retrieveCurrencyRate(baseCurrency: String, toCurrency: String, complition: @escaping (String) -> Void) {
-        self.requestCurrencyRates(baseCurrency: baseCurrency) {[weak self] (data, error) in
-            var string = "No currency retieved!"
-            
-            if let currentError = error {
-                string = currentError.localizedDescription
-            } else {
-                if let strongSelf = self {
-                    string = strongSelf.parseCurrencyRatesResponse(data: data, baseCurrency: baseCurrency, toCurrency: toCurrency)
-                }
-            }
-            
-            complition(string)
-        }
-    }
-    
-    func requestCurrentCurrencyRate() {
-        self.activityIndicator.startAnimating()
-        self.label.text = ""
-        
-        let baseCurrencyIndex = self.pickerFrom.selectedRow(inComponent: 0)
-        let toCurrencyIndex = self.pickerTo.selectedRow(inComponent: 0)
-        
-        let baseCurrency = self.currencies[baseCurrencyIndex]
-        let toCurrency = self.currenciesExceptBase()[toCurrencyIndex]
-        
-        self.retrieveCurrencyRate(baseCurrency: baseCurrency, toCurrency: toCurrency) {[weak self] (value) in
-            DispatchQueue.main.async(execute: {
-                if let strongSelf = self {
-                    if strongSelf.label.text != value {
-                        strongSelf.label.text = value
-                    }
-                    strongSelf.activityIndicator.stopAnimating()
-                    strongSelf.pickerFrom.reloadAllComponents()
-                    strongSelf.pickerTo.reloadAllComponents()
-                }
-            })
-        }
     }
     
     func currenciesExceptBase() -> [String] {
